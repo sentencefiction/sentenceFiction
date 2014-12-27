4214ffd9a44902bc16e30e9ab45cf6d1be2c41c9
sachin <sachin1992sharma@outlook.com>
2014-12-27 21:26:40 +0530

    AddedStoryBack

---
+++ Fiction/Models/StoryModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Fiction.Models
+{
+    public class Story
+    {
+        public Guid id { get; set; }
+        public int type { get; set; }
+        public string story { get; set; }
+        public DateTime dateTimeCreated { get; set; }
+        public int numberOfHearts { get; set; }
+        public int numberOfComments { get; set; }
+        public int numberOfReposts { get; set; }
+        
+    }
+    public class CreateStoryViewModel
+    {
+        [Required]
+        [Display(Name="The type of story")]
+        public int type;
+        [Required]
+        [Display(Name="Story")]
+        public string story;
+        [Display(Name="Name of story")]
+        public string name;
+    }
+    public class StoryUserRelationship
+    {
+        public Guid id { get; set; }
+        public Guid userId { get; set;}
+        public Guid storyId { get; set; }
+    }
+}
